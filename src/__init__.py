c6d51afa14700fb1f498507eac48700d3ad1a108
tnckline-del <tnckline@gmail.com>
2026-09-05 14:17:44 -0400

    Sprint 7 - Initialize RIMS package

---
+++ src/__init__.py
@@ -0,0 +1,19 @@
+"""
+Purpose:
+    Define the RIMS application package.
+
+Responsibilities:
+    - Identify the src directory as the RIMS Python package.
+    - Provide package-level metadata.
+
+Dependencies:
+    Python standard library only.
+
+Revision History:
+    0.2.0 - Initial package definition.
+
+Author:
+    RIMS Development Team
+"""
+
+__version__ = "0.2.0"
